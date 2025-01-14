d96d8076aee8018b993fb3ba3fc22243901cc8fe
Terrance Mann <terrancem29@nycstudents.net5>
2025-01-14 12:44:09 -0500

    part 4.4

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -17,9 +17,13 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 lookDirection = (player.transform.position
-        - transform.position).normalized;
+        Vector3 lookDirection = (player.transform.position - transform.position).normalized;
         
         enemyRb.AddForce(lookDirection * speed);
+
+        if (transform.position.y < -10)
+        {
+            Destroy(gameObject);
+        }
     }
 }
